95a0631de70be71bdcd0c1a1f744e70776c2b5c2
guoyufan <guoyf@yaoyaobar.com>
2016-05-03 18:10:02 +0800

    Initial Commit

---
+++ ImageReview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageReview
+//
+//  Created by gyf on 16/5/3.
+//  Copyright © 2016年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageReview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageReview
+//
+//  Created by gyf on 16/5/3.
+//  Copyright © 2016年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
